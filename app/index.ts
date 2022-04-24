232d037a0150699e63c99e44ca8155701afae57e
P0sseid0n <matheuspossidoniom@outlook.com>
2022-04-24 00:10:16 -0300

    Separando token de bots

--- app/index.ts
+++ app/index.ts
@@ -4,4 +4,4 @@ dotenv.config()
 
 const client = new Client({ intents: ['GUILDS', 'GUILD_MESSAGES'] })
 
-client.start(process.env['BOT_TOKEN'])
+client.start(process.env.NODE_ENV == 'production' ? process.env['PROD_BOT_TOKEN'] : process.env['DEV_BOT_TOKEN'])
