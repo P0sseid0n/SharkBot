f351ef2b4829b7b8e9ef32471a1db66e33da4301
P0sseid0n <matheuspossidoniom@outlook.com>
2022-04-23 23:36:16 -0300

    Criando arquivo do prisma

--- app/src/Commands/Note.ts
+++ app/src/Commands/Note.ts
@@ -1,21 +1,9 @@
 import { CommandFn } from '@/Types'
-import { MessageActionRow, MessageButton, MessageEmbed, TextBasedChannel } from 'discord.js'
+import { MessageActionRow, MessageButton, MessageEmbed } from 'discord.js'
 
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+import db from '@/Database'
 
 const Command: CommandFn = client => {
-	async function getMessageById(id: string, channel: TextBasedChannel) {
-		if (!/[0-9]{18}/.test(id)) return false
-
-		try {
-			const message = await channel.messages.fetch(id)
-			return message
-		} catch {
-			return false
-		}
-	}
-
 	function getMessageId(msg: string) {
 		const regex = /[0-9]{18}/
 		const match = msg.match(regex)
@@ -36,7 +24,7 @@ const Command: CommandFn = client => {
 					title: msg.command.args.filter(arg => !getMessageId(arg)).join(' '),
 				}
 
-				prisma.note
+				db.note
 					.upsert({
 						where: {
 							title_userId: {
@@ -75,7 +63,7 @@ const Command: CommandFn = client => {
 						msg.reply('Ocorreu um erro ao salvar nota. Tente novamente.')
 					})
 			} else {
-				const noteDb = await prisma.note.findFirst({
+				const noteDb = await db.note.findFirst({
 					where: {
 						userId: msg.author.id,
 						title: {
@@ -101,79 +89,8 @@ const Command: CommandFn = client => {
 
 				msg.reply({ content: '**Nota encontrada**', embeds: [embed], components: [row] })
 			}
-
-			/*
-
-			if (msg.command.args.length == 0) return console.log('Args vazio')
-			// msg.reply(msg.url)
-
-			// TODO: Verificar se o msg.command.args[0] é um link para uma mensagem
-			const messageNote = await getMessageById(msg.command.args[0], msg.channel)
-
-			if (messageNote) msg.command.args.shift()
-
-			const title = msg.command.args.join(' ')
-			if (!title) return console.log('Titulo vazio')
-
-			if (messageNote) {
-				const noteDb = await prisma.note.upsert({
-					where: {
-						title,
-					},
-					update: {
-						title,
-					},
-					create: {
-						title,
-						url: messageNote.url,
-						user: {
-							connectOrCreate: {
-								where: {
-									id: msg.author.id,
-								},
-								create: {
-									id: msg.author.id,
-								},
-							},
-						},
-					},
-				})
-
-				const embed = new MessageEmbed().setTitle('Nota salva').setDescription(`
-               \`Titulo\`: ${noteDb.title},
-            `)
-
-				msg.reply({ embeds: [embed] })
-			} else {
-				const noteDb = await prisma.note.findFirst({
-					where: {
-						title: {
-							contains: title,
-						},
-					},
-				})
-
-				if (!noteDb) return await msg.reply('Nota não encontrada')
-
-				const row = new MessageActionRow().addComponents(
-					new MessageButton().setLabel('Ir até a nota').setStyle('LINK').setURL(noteDb.url)
-				)
-
-				const embed = new MessageEmbed().setDescription(`
-               **Id: \`${noteDb.id}\`**
-               **Titulo: \`${noteDb.title}\`**
-            `)
-
-				msg.reply({ embeds: [embed], components: [row] })
-			}
-         */
 		},
 	}
 }
 
 export default Command
-
-/*
- 1. Ver se o usuario está respondendo a uma mensagem
- 2. Ver
-*/
